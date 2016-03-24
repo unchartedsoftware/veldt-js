9e7756137e32411b98da95bf980c371a8ce437a6
kbirk <birk.kevin@gmail.com>
2016-03-23 22:59:05 -0400

    Fix tile hashing bug, use json-stable-stringify instead.

--- scripts/request/TileRequestor.js
+++ scripts/request/TileRequestor.js
@@ -2,7 +2,26 @@
 
     'use strict';
 
+    var stringify = require('json-stable-stringify');
     var Requestor = require('./Requestor');
+
+    function pruneEmpty(obj) {
+        return function prune(current) {
+            _.forOwn(current, function(value, key) {
+              if (_.isUndefined(value) || _.isNull(value) || _.isNaN(value) ||
+                (_.isString(value) && _.isEmpty(value)) ||
+                (_.isObject(value) && _.isEmpty(prune(value)))) {
+                delete current[key];
+              }
+            });
+            // remove any leftover undefined values from the delete
+            // operation on an array
+            if (_.isArray(current)) {
+                _.pull(current, undefined);
+            }
+            return current;
+        }(_.cloneDeep(obj)); // do not modify the original object, create a clone instead
+    }
 
     function TileRequestor() {
         Requestor.apply(this, arguments);
@@ -12,27 +31,14 @@
 
     TileRequestor.prototype.getHash = function(req) {
         var coord = req.coord;
-        var params = [];
-        _.forIn(req.params, function(param, paramName) {
-            if (param) {
-                var strs = [paramName];
-                _.forIn(param, function(val, key) {
-                    if (val !== undefined && val !== null) {
-                        strs.push(key.toLowerCase() + '=' + val);
-                    }
-                });
-                strs.sort();
-                params.push(strs.join(','));
-            }
-        });
-        params.sort();
+        var hash = stringify(pruneEmpty(req.params));
         return req.type + '-' +
             req.index + '-' +
             req.store + '-' +
             coord.x + '-' +
             coord.y + '-' +
             coord.z + '-' +
-            params.join('-');
+            hash;
     };
 
     TileRequestor.prototype.getURL = function(res) {
