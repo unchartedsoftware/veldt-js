a1c71662f955f145485c05243c5c5ad94eb8886d
cmenezes <cmenezes@uncharted.software>
2016-04-04 17:47:24 -0400

    add skeleton for boolQuery. add dep to Heatmap layer

---
+++ scripts/layer/params/BoolQuery.js
@@ -0,0 +1,37 @@
+(function(){
+
+  'use strict';
+
+  var checkField = function(meta, field) {
+    if (meta && meta.type) {
+
+    }
+  }
+
+  function addBoolQuery(query){
+
+    // do some validation on the query object
+    // e.g. make sure that all of the fields referenced are actually part of the metadata for the layer
+    this.boolQuery = query;
+    console.log('adding forreals');
+    return {};
+  }
+
+  function removeBoolQuery(){
+    this.boolQuery = null;
+    console.log('removing forreals');
+    return {};
+  }
+
+  function getBoolQuery(){
+
+    console.log('getting forreals');
+    return this.boolQuery;
+  }
+
+  module.exports = {
+    addBoolQuery : addBoolQuery,
+    removeBoolQuery : removeBoolQuery,
+    getBoolQuery : getBoolQuery
+  }
+})
