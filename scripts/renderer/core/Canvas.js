fea23177dc9d5f1dd78f947e47d8e90dfde92813
kbirk <birk.kevin@gmail.com>
2016-04-29 14:54:22 -0400

    Added optional handlers interface to canvas layer.

--- scripts/renderer/core/Canvas.js
+++ scripts/renderer/core/Canvas.js
@@ -10,12 +10,26 @@
         },
 
         onAdd: function(map) {
+            var self = this;
             DOM.prototype.onAdd.call(this, map);
             map.on('click', this.onClick, this);
+            $(this._container).on('mousemove', function(e) {
+                self.onMouseMove(e);
+            });
+            $(this._container).on('mouseover', function(e) {
+                self.onMouseOver(e);
+            });
+            $(this._container).on('mouseout', function(e) {
+                self.onMouseOut(e);
+            });
         },
 
         onRemove: function(map) {
             map.off('click', this.onClick, this);
+            $(this._container).off('mousemove');
+            $(this._container).off('mouseover');
+            $(this._container).off('mouseout');
+            DOM.prototype.onRemove.call(this, map);
         },
 
         _createTile: function() {
@@ -25,9 +39,22 @@
             return tile;
         },
 
+        onMouseMove: function() {
+            // override
+        },
+
+        onMouseOver: function() {
+            // override
+        },
+
+        onMouseOut: function() {
+            // override
+        },
+
         onClick: function() {
             // override
         }
+
     });
 
     module.exports = Canvas;
