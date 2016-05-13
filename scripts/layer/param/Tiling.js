b4a08aadaa7088ec349b4e42d1377ceb1dd18fbd
kbirk <birk.kevin@gmail.com>
2016-05-13 15:50:36 -0400

    Add methods for converting from data coordinates and layer pixels.

--- scripts/layer/param/Tiling.js
+++ scripts/layer/param/Tiling.js
@@ -4,6 +4,8 @@
 
     var DEFAULT_X_FIELD = 'pixel.x';
     var DEFAULT_Y_FIELD = 'pixel.y';
+    var DEFAULT_PIXEL_MIN = 0;
+    var DEFAULT_PIXEL_MAX = 1 << 32;
 
     var checkField = function(meta, field) {
         if (meta) {
@@ -22,9 +24,9 @@
         if (field !== this._params.binning.x) {
             if (field === DEFAULT_X_FIELD) {
                 // reset if default
-                this._params.binning.x = undefined;
-                this._params.binning.left = undefined;
-                this._params.binning.right = undefined;
+                this._params.binning.x = DEFAULT_X_FIELD;
+                this._params.binning.left = DEFAULT_PIXEL_MIN;
+                this._params.binning.right = DEFAULT_PIXEL_MAX;
                 this.clearExtrema();
             } else {
                 var meta = this._meta[field];
@@ -47,9 +49,9 @@
         if (field !== this._params.binning.y) {
             if (field === DEFAULT_Y_FIELD) {
                 // reset if default
-                this._params.binning.y = undefined;
-                this._params.binning.bottom = undefined;
-                this._params.binning.top = undefined;
+                this._params.binning.y = DEFAULT_Y_FIELD;
+                this._params.binning.bottom = DEFAULT_PIXEL_MAX;
+                this._params.binning.top = DEFAULT_PIXEL_MIN;
                 this.clearExtrema();
             } else {
                 var meta = this._meta[field];
@@ -68,13 +70,66 @@
         return this._params.binning.y;
     };
 
+    var getLayerPointFromDataPoint = function(x, y, zoom) {
+        var binning = this._params.binning;
+        var tileSize = this.options.tileSize;
+        var pow = Math.pow(2, zoom);
+        var extent = tileSize * pow;
+        var xRange = Math.abs(binning.right - binning.left);
+    	var yRange = Math.abs(binning.bottom - binning.top);
+        var nx, ny;
+        if (binning.left > binning.right) {
+            nx = 1 - ((x - binning.right) / xRange);
+    	} else {
+            nx = (x - binning.left) / xRange;
+    	}
+        if (binning.top > binning.bottom) {
+            ny = 1 - ((y - binning.bottom) / yRange);
+    	} else {
+            ny = (y - binning.top) / yRange;
+    	}
+        return {
+            x: extent * nx,
+            y: extent * ny
+        };
+    };
+
+    var getDataPointFromLayerPoint = function(x, y, zoom) {
+        var binning = this._params.binning;
+        var tileSize = this.options.tileSize;
+        var pow = Math.pow(2, zoom);
+        var extent = tileSize * pow;
+        var nx = x / extent;
+        var ny = y / extent;
+        var xRange = Math.abs(binning.right - binning.left);
+    	var yRange = Math.abs(binning.bottom - binning.top);
+        var px, py;
+        if (binning.left > binning.right) {
+            px = binning.right + (1 - nx) * xRange;
+    	} else {
+            px = binning.left + nx * xRange;
+    	}
+        if (binning.top > binning.bottom) {
+            py = binning.bottom + (1 - ny) * yRange;
+    	} else {
+            py = binning.top + ny * yRange;
+    	}
+        return {
+            x: px,
+            y: py
+        };
+    };
+
     module.exports = {
         setXField: setXField,
         getXField: getXField,
         setYField: setYField,
         getYField: getYField,
+        getLayerPointFromDataPoint: getLayerPointFromDataPoint,
+        getDataPointFromLayerPoint: getDataPointFromLayerPoint,
         DEFAULT_X_FIELD: DEFAULT_X_FIELD,
-        DEFAULT_Y_FIELD: DEFAULT_Y_FIELD
+        DEFAULT_Y_FIELD: DEFAULT_Y_FIELD,
+        DEFAULT_PIXEL_MAX: DEFAULT_PIXEL_MAX
     };
 
 }());
