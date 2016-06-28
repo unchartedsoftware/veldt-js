286c95fca883968c3dedb5881152dce0edc92032
kbirk <birk.kevin@gmail.com>
2016-06-27 21:00:18 -0400

    Add random animation offset to delayed pending tile renderers.

--- scripts/renderer/type/pending/DelayedBlink.js
+++ scripts/renderer/type/pending/DelayedBlink.js
@@ -2,15 +2,16 @@
 
     'use strict';
 
-    var DELAY = 1200 / 2;
+    var DELAY = 1200;
 
     module.exports = {
 
         renderTile: function(elem) {
             setTimeout(
                 function() {
+                    var delay = -(Math.random() * DELAY) + 'ms';
                     if ($(elem).hasClass('pending')) {
-                        elem.innerHTML = '<div class="blinking blinking-tile"></div>';
+                        elem.innerHTML = '<div class="blinking blinking-tile" style="animation-delay:' + delay + '"></div>';
                     }
                 },
                 DELAY);
