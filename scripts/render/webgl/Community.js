1a26322b1cf2e0b547851ab47d7ba854223a5bfd
kbirk <birk.kevin@gmail.com>
2017-01-04 15:12:20 -0500

    Added basic community ring renderer.

--- scripts/render/webgl/Community.js
+++ scripts/render/webgl/Community.js
@@ -4,62 +4,148 @@ const defaultTo = require('lodash/defaultTo');
 const lumo = require('lumo');
 const Shaders = require('./Shaders');
 
-const createPoint = function(gl) {
-	const vertices = new Float32Array(2);
-	vertices[0] = 0.0;
-	vertices[1] = 0.0;
-	vertices[2] = 0.0;
-	// create quad buffer
+const NUM_SEGMENTS = 64;
+const RADIUS_OFFSET = 10;
+
+const createRing = function(gl, numSegments, radius, ringWidth) {
+	const theta = (2 * Math.PI) / numSegments;
+	// pre-calculate sine and cosine
+	const c = Math.cos(theta);
+	const s = Math.sin(theta);
+	// start at angle = 0
+	let x0 = 0;
+	let y0 = radius - (ringWidth / 2);
+	let x1 = 0;
+	let y1 = radius + (ringWidth / 2);
+	const vertices = new Float32Array((numSegments + 1) * (2 + 2));
+	for (let i = 0; i <= numSegments; i++) {
+		vertices[i*4] = x0;
+		vertices[i*4+1] = y0;
+		vertices[i*4+2] = x1;
+		vertices[i*4+3] = y1;
+		// apply the rotation
+		let t = x0;
+		x0 = c * x0 - s * y0;
+		y0 = s * t + c * y0;
+		t = x1;
+		x1 = c * x1 - s * y1;
+		y1 = s * t + c * y1;
+	}
 	return new lumo.VertexBuffer(
 		gl,
 		vertices,
 		{
-			// position
+			// x, y
 			0: {
 				size: 2,
 				type: 'FLOAT'
-			},
-			// radius
-			1: {
-				size: 1,
-				type: 'FLOAT'
 			}
-		},
-		{
-			mode: 'POINTS',
-			count: 1
+		}, {
+			mode: 'TRIANGLE_STRIP',
+			count: vertices.length / 2
 		});
 };
 
-const renderTiles = function(gl, shader, atlas, plot, layer, proj, renderables, color) {
+/*
+const renderTiles = function(shader, ring, ding, atlas, proj, renderables, color, opacity) {
+	// use shader
+	shader.use();
 
-	// clear render target
-	gl.clear(gl.COLOR_BUFFER_BIT);
-
-	// set blending func
-	gl.enable(gl.BLEND);
-	gl.blendFunc(gl.SRC_ALPHA, gl.ONE);
-
+	// set uniforms
+	shader.setUniform('uProjectionMatrix', proj);
 	shader.setUniform('uColor', color);
-	shader.setUniform('uRadiusOffset', 0);
+	shader.setUniform('uOpacity', opacity);
+	shader.setUniform('uRadiusOffset', RADIUS_OFFSET);
 
-	// binds the buffer to instance
-	atlas.bind();
+	// bind the ring buffer
+	ring.bind();
+
+	// binds instance offset buffer
+	atlas.bindInstanced();
 
 	// for each renderable
 	renderables.forEach(renderable => {
 		// set tile uniforms
 		shader.setUniform('uScale', renderable.scale);
 		shader.setUniform('uTileOffset', renderable.tileOffset);
-		// draw the points
-		atlas.draw(renderable.hash, 'POINTS');
+		// draw the instances
+		atlas.drawInstanced(renderable.hash, ring.mode, ring.count);
 	});
 
-	// unbind
-	atlas.unbind();
+	// unbind instance offset buffer
+	atlas.unbindInstanced();
+
+	// unbind the ring buffer
+	ring.unbind();
+};
+*/
+
+const renderTiles = function(shader, fill, outline, atlas, proj, renderables, color, outlineColor, opacity) {
+	// use shader
+	shader.use();
+
+	// set uniforms
+	shader.setUniform('uProjectionMatrix', proj);
+	shader.setUniform('uRadiusOffset', RADIUS_OFFSET);
+	shader.setUniform('uOpacity', opacity);
+
+	/*
+	 * Draw outlineWidth
+	 */
+
+	// bind the ring buffer
+	outline.bind();
+
+	// binds instance offset buffer
+	atlas.bindInstanced();
+
+	// set color
+	shader.setUniform('uColor', outlineColor);
+
+	renderables.forEach(renderable => {
+		// set tile uniforms
+		shader.setUniform('uScale', renderable.scale);
+		shader.setUniform('uTileOffset', renderable.tileOffset);
+		// draw the instances
+		atlas.drawInstanced(renderable.hash, outline.mode, outline.count);
+	});
+
+	// unbind instance offset buffer
+	atlas.unbindInstanced();
+
+	// unbind the ring buffer
+	outline.unbind();
+
+	/*
+	 * Draw fill
+	 */
+
+	// bind the ring buffer
+	fill.bind();
+
+	// binds instance offset buffer
+	atlas.bindInstanced();
+
+	// set color
+	shader.setUniform('uColor', color);
+
+	// for each renderable
+	renderables.forEach(renderable => {
+		// set tile uniforms
+		shader.setUniform('uScale', renderable.scale);
+		shader.setUniform('uTileOffset', renderable.tileOffset);
+		// draw the instances
+		atlas.drawInstanced(renderable.hash, fill.mode, fill.count);
+	});
+
+	// unbind instance offset buffer
+	atlas.unbindInstanced();
+
+	// unbind the ring buffer
+	fill.unbind();
 };
 
-const renderPoint = function(gl, point, shader, proj, plot, target, color) {
+const renderRing = function(shader, fill, proj, plot, target, color, opacity) {
 
 	// get tile offset
 	const coord = target.tile.coord;
@@ -68,19 +154,25 @@ const renderPoint = function(gl, point, shader, proj, plot, target, color) {
 		(coord.x * scale * plot.tileSize) + (scale * target.x) - plot.viewport.x,
 		(coord.y * scale * plot.tileSize) + (scale * target.y) - plot.viewport.y
 	];
+
+	// use shader
+	shader.use();
+
+	// set uniforms
+	shader.setUniform('uProjectionMatrix', proj);
 	shader.setUniform('uColor', color);
+	shader.setUniform('uOpacity', opacity);
+	shader.setUniform('uRadius', target.radius);
+	shader.setUniform('uRadiusOffset', RADIUS_OFFSET);
 	shader.setUniform('uScale', scale);
 	shader.setUniform('uTileOffset', tileOffset);
-	shader.setUniform('uRadiusOffset', target.radius);
 
-	// binds the buffer to instance
-	point.bind();
-
-	// draw the points
-	point.draw();
-
-	// unbind
-	point.unbind();
+	// bind the ring buffer
+	fill.bind();
+	// draw ring
+	fill.draw();
+	// unbind the ring buffer
+	fill.unbind();
 };
 
 class Community extends lumo.WebGLInteractiveRenderer {
@@ -88,13 +180,60 @@ class Community extends lumo.WebGLInteractiveRenderer {
 	constructor(options = {}) {
 		super(options);
 		this.shader = null;
-		this.point = null;
+		this.instancedShader = null;
+		this.ringFill = null;
+		this.ringOutline = null;
 		this.atlas = null;
-		this.color = defaultTo(options.color, [ 1.0, 1.0, 1.0, 0.8 ]);
+		this.color = defaultTo(options.color, [ 1.0, 1.0, 1.0, 1.0 ]);
+		this.outlineColor = defaultTo(options.outlineColor, [ 0.0, 0.0, 0.0, 1.0 ]);
 		this.highlightedColor = defaultTo(options.highlightedColor, [ 1.0, 0.5, 1.0, 0.8 ]);
 		this.selectedColor = defaultTo(options.selectedColor, [ 1.0, 0.5, 1.0, 0.8 ]);
-		this.outline = defaultTo(options.outline, 2);
+		this.ringWidth = defaultTo(options.ringWidth, 2);
+		this.outlineWidth = defaultTo(options.outlineWidth, 1);
 		this.radiusField = defaultTo(options.radiusField, 'radius');
+	}
+
+	onAdd(layer) {
+		super.onAdd(layer);
+		// geometry
+		this.ringFill = createRing(
+			this.gl,
+			NUM_SEGMENTS,
+			RADIUS_OFFSET,
+			this.ringWidth);
+		this.ringOutline = createRing(
+			this.gl,
+			NUM_SEGMENTS,
+			RADIUS_OFFSET,
+			this.ringWidth + (this.outlineWidth * 2));
+		// shaders
+		this.shader = this.createShader(Shaders.ring);
+		this.instancedShader = this.createShader(Shaders.instancedRing);
+		// offset atlas
+		this.atlas = this.createVertexAtlas({
+			// offset
+			1: {
+				size: 2,
+				type: 'FLOAT'
+			},
+			// radius
+			2: {
+				size: 1,
+				type: 'FLOAT'
+			}
+		});
+		return this;
+	}
+
+	onRemove(layer) {
+		this.destroyVertexAtlas(this.atlas);
+		this.shader = null;
+		this.instancedShader = null;
+		this.atlas = null;
+		this.ringFill = null;
+		this.ringOutline = null;
+		super.onRemove(layer);
+		return this;
 	}
 
 	addTile(atlas, tile) {
@@ -109,7 +248,7 @@ class Community extends lumo.WebGLInteractiveRenderer {
 		const radiusField = this.radiusField;
 
 		const radiusScale = Math.pow(2, coord.z);
-		const outlineOffset = this.outline;
+		const outlineOffset = this.outlineWidth;
 
 		const points = new Array(positions.length / 2);
 		const vertices = new Float32Array((positions.length / 2) * 3);
@@ -152,93 +291,62 @@ class Community extends lumo.WebGLInteractiveRenderer {
 		this.removePoints(coord);
 	}
 
-	onAdd(layer) {
-		super.onAdd(layer);
-		// get the extension for standard derivatives
-		this.ext = this.gl.getExtension('OES_standard_derivatives');
-		this.point = createPoint(this.gl);
-		this.shader = this.createShader(Shaders.communityRing);
-		this.atlas = this.createVertexAtlas({
-			// position
-			0: {
-				size: 2,
-				type: 'FLOAT'
-			},
-			// radius
-			1: {
-				size: 1,
-				type: 'FLOAT'
-			}
-		});
-		return this;
-	}
-
-	onRemove(layer) {
-		this.destroyVertexAtlas(this.atlas);
-		this.shader = null;
-		this.atlas = null;
-		this.point = null;
-		super.onRemove(layer);
-		return this;
-	}
-
 	draw() {
 
+		const gl = this.gl;
 		const layer = this.layer;
 		const plot = layer.plot;
 		const proj = this.getOrthoMatrix();
-		const shader = this.shader;
 
-		// bind render target
-		plot.renderBuffer.bind();
+		// // bind render target
+		// plot.renderBuffer.bind();
+		// plot.renderBuffer.clear();
 
-		// use shader
-		shader.use();
+		// set blending func
+		gl.enable(gl.BLEND);
+		gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
 
-		// set uniforms
-		shader.setUniform('uProjectionMatrix', proj);
-		shader.setUniform('uPixelRatio', plot.pixelRatio);
-
-		// render the tiles
 		renderTiles(
-			this.gl,
-			shader,
+			this.instancedShader,
+			this.ringFill,
+			this.ringOutline,
 			this.atlas,
-			plot,
-			layer,
 			proj,
 			this.getRenderables(),
-			this.color);
+			this.color,
+			this.outlineColor,
+			layer.opacity);
 
 		// render selected
 		if (this.selected) {
-			renderPoint(
-				this.gl,
-				this.point,
-				shader,
+			renderRing(
+				this.shader,
+				this.ringFill,
 				proj,
 				plot,
 				this.selected,
-				this.selectedColor);
+				this.selectedColor,
+				layer.opacity);
 		}
 
 		// render highlighted
 		if (this.highlighted && this.highlighted !== this.selected) {
-			renderPoint(
-				this.gl,
-				this.point,
-				shader,
+			renderRing(
+				this.shader,
+				this.ringFill,
 				proj,
 				plot,
 				this.highlighted,
-				this.highlightedColor);
+				this.highlightedColor,
+				layer.opacity);
 		}
 
-		// unbind render target
-		plot.renderBuffer.unbind();
+		// // unbind render target
+		// plot.renderBuffer.unbind();
+		//
+		// // render framebuffer to the backbuffer
+		// plot.renderBuffer.blitToScreen(this.layer.opacity);
 
-		// render framebuffer to the backbuffer
-		plot.renderBuffer.blitToScreen(this.layer.opacity);
 		return this;
 	}
 
--- scripts/render/webgl/Shaders.js
+++ scripts/render/webgl/Shaders.js
@@ -257,51 +257,58 @@ const macro = {
 		`
 };
 
-
-/**
- * community ring shader
- */
-const communityRing = {
+const ring = {
 	vert:
 		precision +
 		`
 		attribute vec2 aPosition;
-		attribute float aRadius;
+		uniform float uRadius;
 		uniform vec2 uTileOffset;
 		uniform float uScale;
-		uniform float uPixelRatio;
 		uniform float uRadiusOffset;
 		uniform mat4 uProjectionMatrix;
-		//varying float vRadius;
 		void main() {
-			vec2 wPosition = (aPosition * uScale) + uTileOffset;
-			gl_PointSize = (aRadius + uRadiusOffset) * uScale * 2.0 * uPixelRatio;
+			vec2 wPosition = (aPosition * uScale) +
+				uTileOffset +
+				(normalize(aPosition) * (uRadius - uRadiusOffset));
 			gl_Position = uProjectionMatrix * vec4(wPosition, 0.0, 1.0);
-			//vRadius = aRadius;
 		}
 		`,
 	frag:
 		precision +
 		`
-		#ifdef GL_OES_standard_derivatives
-			#extension GL_OES_standard_derivatives : enable
-		#endif
 		uniform vec4 uColor;
-		uniform float uOutline;
-		//varying float vRadius;
+		uniform float uOpacity;
 		void main() {
-			vec2 cxy = 2.0 * gl_PointCoord - 1.0;
-			float radius = dot(cxy, cxy);
-			float alpha = 1.0;
-			#ifdef GL_OES_standard_derivatives
-				float delta = fwidth(radius);
-				alpha = 1.0 - smoothstep(1.0 - delta, 1.0 + delta, radius);
-			#else
-				if (radius > 1.0) {
-					discard;
-				}
-			#endif
-			gl_FragColor = vec4(uColor.rgb, uColor.a * alpha);
+			gl_FragColor = vec4(uColor.rgb, uColor.a * uOpacity);
+		}
+		`
+};
+
+const instancedRing = {
+	vert:
+		precision +
+		`
+		attribute vec2 aPosition;
+		attribute vec2 aOffset;
+		attribute float aRadius;
+		uniform vec2 uTileOffset;
+		uniform float uScale;
+		uniform float uRadiusOffset;
+		uniform mat4 uProjectionMatrix;
+		void main() {
+			vec2 radiusOffset = normalize(aPosition) * (aRadius - uRadiusOffset);
+			vec2 wPosition = ((aPosition + radiusOffset + aOffset) * uScale) + uTileOffset;
+			gl_Position = uProjectionMatrix * vec4(wPosition, 0.0, 1.0);
+		}
+		`,
+	frag:
+		precision +
+		`
+		uniform vec4 uColor;
+		uniform float uOpacity;
+		void main() {
+			gl_FragColor = vec4(uColor.rgb, uColor.a * uOpacity);
 		}
 		`
 };
@@ -323,7 +330,8 @@ module.exports = {
 	macro: macro,
 
 	/**
-	 * community ring shader
+	 * community shaders
 	 */
-	communityRing: communityRing
+	ring: ring,
+	instancedRing: instancedRing
 };
