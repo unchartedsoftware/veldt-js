a630e3d03eaa6b46e9da89cf34a59b77abd6aa4e
Derek Gray <github@mudflye.org>
2017-03-30 15:03:45 -0400

    Pull out a Base layer so that static layers don't need to extend "Live"

--- scripts/layer/core/Live.js
+++ scripts/layer/core/Live.js
@@ -4,104 +4,15 @@ const lumo = require('lumo');
 const defaultTo = require('lodash/defaultTo');
 const isEmpty = require('lodash/isEmpty');
 const isFunction = require('lodash/isFunction');
+const Base = require('./Base');
 
-const TILE_ADD = Symbol();
-
-const REDRAW_TIMEOUT_MS = 800;
-
-class Live extends lumo.Layer {
+class Live extends Base {
 
 	constructor(meta, options = {}) {
 		super(options);
 		this.meta = meta;
-		this.params = {};
 		this.query = null;
 		this.filters = new Map();
-		this.transform = defaultTo(options.transform, null);
-		this.redrawDebounce = null;
-		this.handlers = new Map();
-		// set extrema / cache
-		this.clearExtrema();
-	}
-
-	onAdd(plot) {
-		// create handler
-		const add = event => {
-			if (this.transform) {
-				event.tile.data = this.transform(event.tile.data);
-			}
-			const updated = this.updateExtrema(event.tile.coord, event.tile.data);
-			if (updated && this.renderer && this.renderer.redraw) {
-				clearTimeout(this.redrawDebounce);
-				this.redrawDebounce = setTimeout(() => {
-					if (this.renderer && this.renderer.redraw) {
-						this.renderer.redraw(true);
-					}
-					// clear debounce
-					this.redrawDebounce = null;
-				}, REDRAW_TIMEOUT_MS);
-			}
-		};
-		// attach handler
-		// NOTE: add this BEFORE calling super, this NEEDS to be the first
-		// `TILE_ADD` callback.
-		this.on(lumo.TILE_ADD, add);
-		// store handler
-		this.handlers.set(TILE_ADD, add);
-		super.onAdd(plot);
-		return this;
-	}
-
-	onRemove(plot) {
-		// clear any pending timeout
-		clearTimeout(this.redrawDebounce);
-		this.redrawDebounce = null;
-		// detach handler
-		this.removeListener(lumo.TILE_ADD, this.handlers.get(TILE_ADD));
-		// delete handler
-		this.handlers.delete(TILE_ADD);
-		super.onRemove(plot);
-		return this;
-	}
-
-	clearExtrema() {
-		this.extremas = new Map();
-	}
-
-	getExtrema(level = Math.round(this.plot.zoom)) {
-		let extrema = null;
-		if (!this.extremas.has(level)) {
-			extrema = {
-				min: Infinity,
-				max: -Infinity
-			};
-			this.extremas.set(level, extrema);
-		} else {
-			extrema = this.extremas.get(level);
-		}
-		return extrema;
-	}
-
-	updateExtrema(coord, data) {
-		const current = this.getExtrema(coord.z);
-		const extrema = this.extractExtrema(data);
-		let changed = false;
-		if (extrema.min < current.min) {
-			changed = true;
-			current.min = extrema.min;
-		}
-		if (extrema.max > current.max) {
-			changed = true;
-			current.max = extrema.max;
-		}
-		return changed;
-	}
-
-	extractExtrema() {
-		return {
-			min: Infinity,
-			max: -Infinity
-		};
 	}
 
 	addFilter(id, filter) {
@@ -157,9 +68,9 @@ class Live extends lumo.Layer {
 		return this.meta;
 	}
 
-	getParams() {
-		return this.params;
-	}
+	//getParams() {
+	//	return this.params;
+	//}
 }
 
 module.exports = Live;
