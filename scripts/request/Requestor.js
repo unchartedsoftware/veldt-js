f26358519b1b8cb563ca9967c251d56fb15d653d
kbirk <birk.kevin@gmail.com>
2017-04-25 21:02:10 -0400

    Remove jquery deferreds and use promises instead in Requestor. Fix bug on connection loss and reconnection.

--- scripts/request/Requestor.js
+++ scripts/request/Requestor.js
@@ -1,10 +1,15 @@
 'use strict';
 
-const _ = require('lodash');
-const $ = require('jquery');
+const cloneDeep = require('lodash/cloneDeep');
+const forIn = require('lodash/forIn');
+const isArray = require('lodash/isArray');
+const isEmpty = require('lodash/isEmpty');
+const isObject = require('lodash/isObject');
+const isString = require('lodash/isString');
+const pull = require('lodash/pull');
 const stringify = require('json-stable-stringify');
 
-const RETRY_INTERVAL = 5000;
+const RETRY_INTERVAL_MS = 5000;
 
 function getHost() {
 	const loc = window.location;
@@ -17,130 +22,141 @@ function establishConnection(requestor, callback) {
 	// on open
 	requestor.socket.onopen = function() {
 		requestor.isOpen = true;
-		console.log('Websocket connection established');
+		console.log(`WebSocket connection established on /${requestor.url}`);
 		callback(null, requestor);
 	};
 	// on message
 	requestor.socket.onmessage = function(event) {
 		const res = JSON.parse(event.data);
-		// save success and error here, as we need to remove them to hash
+		// NOTE: save success and error here, as we need to remove them to hash
 		// correctly
 		const success = res.success;
 		const error = res.error;
-		const hash = requestor.getHash(res);
-		const request = requestor.requests.get(hash);
+		const hash = requestor.getHash(res, false);
+		if (!requestor.requests.has(hash)) {
+			console.error('Unrecognized response: ', res,  ', discarding');
+		}
+		const wrapped = requestor.requests.get(hash);
 		requestor.requests.delete(hash);
 		if (success) {
-			request.resolve(requestor.getURL());
+			wrapped.resolve(requestor.getURL());
 		} else {
-			request.reject(new Error(error));
+			wrapped.reject(new Error(error));
 		}
 	};
 	// on close
 	requestor.socket.onclose = function() {
 		// log close only if connection was ever open
 		if (requestor.isOpen) {
-			console.warn('Websocket connection closed, attempting to re-connect in', RETRY_INTERVAL);
+			console.warn(`WebSocket connection on /${requestor.url} lost, attempting to reconnect in ${RETRY_INTERVAL_MS}ms`);
 		}
 		requestor.socket = null;
 		requestor.isOpen = false;
 		// reject all current requests
-		requestor.requests.forEach(function(key, request) {
-			request.reject();
+		requestor.requests.forEach(wrapped => {
+			wrapped.reject();
 		});
 		// clear request map
 		requestor.requests = new Map();
 		// attempt to re-establish connection
-		setTimeout(function() {
-			establishConnection(requestor, function() {
+		setTimeout(() => {
+			establishConnection(requestor, () => {
 				// once connection is re-established, send pending requests
-				requestor.pending.forEach(function(key, pending) {
-					const request = pending.request;
-					const deferred = pending.deferred;
-					const hash = pending.hash;
-					requestor.requests.set(hash, deferred);
-					requestor.socket.send(JSON.stringify(request));
+				requestor.pending.forEach(wrapped => {
+					requestor.requests.set(wrapped.hash, wrapped);
+					requestor.socket.send(JSON.stringify(wrapped.request));
 				});
 				// clear pending map
 				requestor.pending = new Map();
 			});
-		}, RETRY_INTERVAL);
+		}, RETRY_INTERVAL_MS);
 	};
 }
 
 function prune(current) {
-	_.forOwn(current, (value, key) => {
-		if (_.isUndefined(value) ||
-			_.isNull(value) ||
-			_.isNaN(value) ||
-			(_.isString(value) && _.isEmpty(value)) ||
-			(_.isObject(value) && _.isEmpty(prune(value)))) {
+	forIn(current, (value, key) => {
+		if (value === undefined ||
+			value == null ||
+			(isString(value) && isEmpty(value)) ||
+			(isObject(value) && isEmpty(prune(value)))) {
 			delete current[key];
 		}
 	});
 	// remove any leftover undefined values from the delete
 	// operation on an array
-	if (_.isArray(current)) {
-		_.pull(current, undefined);
+	if (isArray(current)) {
+		pull(current, undefined);
 	}
 	return current;
 }
 
-function pruneEmpty(obj) {
-	// do not modify the original object, create a clone instead
-	return prune(_.cloneDeep(obj));
+function pruneEmpty(obj, clone) {
+	return clone ? prune(cloneDeep(obj)) : prune(obj);
 }
 
-function hashReq(req) {
-	req.error = undefined;
-	req.success = undefined;
-	return stringify(pruneEmpty(req));
+function wrappedPromise(hash, request) {
+	let resolve;
+	let reject;
+	const promise = new Promise((res, rej) => {
+		resolve = res;
+		reject = rej;
+	});
+	return {
+		hash: hash,
+		request: request,
+		promise: promise,
+		resolve: resolve,
+		reject: reject
+	};
 }
 
 class Requestor {
 	constructor(url, callback) {
-		this.url = url;
+		this.url = (url[0] === '/') ? url.substring(1, url.length - 1) : url;
 		this.requests = new Map();
 		this.pending = new Map();
 		this.isOpen = false;
 		establishConnection(this, callback);
 	}
 	getHash(req) {
-		return hashReq(req);
+		// NOTE: need to remove the success / error attributes otherwise
+		// the request hashes wont match the response hashes.
+		req.error = undefined;
+		req.success = undefined;
+		return stringify(pruneEmpty(req));
 	}
 	getURL() {
 		return this.url;
 	}
 	get(req) {
-		const hash = this.getHash(req);
+		const hash = this.getHash(req, true);
 		if (!this.isOpen) {
+			// see if we already have a pending request
 			let pending = this.pending.get(hash);
 			if (pending) {
-				return pending.deferred.promise();
+				return pending.promise;
 			}
-			// if no connection, add request to pending queue
-			const deferred = new $.Deferred();
-			pending = {
-				hash: hash,
-				request: req,
-				deferred: deferred
-			};
+			// if no pending request, add wrapped promise to pending queue
+			pending = wrappedPromise(hash, req);
 			this.pending.set(hash, pending);
-			return deferred.promise();
+			return pending.promise;
 		}
-		let deferred = this.requests.get(hash);
-		if (deferred) {
-			return deferred.promise();
+		// see if we already have a request
+		let wrapped = this.requests.get(hash);
+		if (wrapped) {
+			return wrapped.promise;
 		}
-		deferred = new $.Deferred();
-		this.requests.set(hash, deferred);
-		this.socket.send(JSON.stringify(req));
-		return deferred.promise();
+		// if no existing reuqest, create wrapped promise and add to map
+		wrapped = wrappedPromise(hash, req);
+		this.requests.set(hash, wrapped);
+		this.socket.send(JSON.stringify(wrapped.request));
+		return wrapped.promise;
 	}
 	close() {
 		this.socket.onclose = null;
 		this.socket.close();
 		this.socket = null;
+		console.warn(`WebSocket connection on /${this.url} closed`);
 	}
 }
 
