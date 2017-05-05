ddf3a8a76a342a111197c4737d9a8108b12516f8
kbirk <birk.kevin@gmail.com>
2017-05-05 00:01:13 -0400

    Remove extended DOMRenderer timeouts.

--- scripts/render/dom/DOMRenderer.js
+++ scripts/render/dom/DOMRenderer.js
@@ -6,8 +6,8 @@ const EventType = require('../event/EventType');
 const CELL_UPDATE = Symbol();
 const DRAW_TIMEOUT = Symbol();
 const ERASE_TIMEOUT = Symbol();
-const DRAW_DEBOUNCE_MS = 4000;
-const ERASE_DEBOUNCE_MS = 4000;
+const DRAW_DEBOUNCE_MS = 400;
+const ERASE_DEBOUNCE_MS = 400;
 const OPACITY_TIMEOUT_MS = 40;
 const OPACITY_FADE_IN_MS = 400;
 
